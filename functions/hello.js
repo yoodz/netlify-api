3a44ca9e3b4cf06a2f376509e5c0f3a6c2fa1d56
yoodz <904201050@qq.com>
2025-02-03 21:25:19 +0800

    测试通知

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,6 @@
 exports.handler = async (event, context) => {
     return {
-        statusCode: 200, body: JSON.stringify({ message: 'Hello from Netlify!' }),
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Hello from Netlify!' })
     };
 };
