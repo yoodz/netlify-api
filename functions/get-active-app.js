e83f3a61fedd7b46dd0b83ed6ed10f6f9fec4ec4
yoodz <904201050@qq.com>
2025-02-06 00:01:44 +0800

    1

--- functions/get-active-app.js
+++ functions/get-active-app.js
@@ -49,7 +49,7 @@ exports.handler = async (event, context) => {
         return {
             statusCode: 200,
             headers,
-            body: JSON.stringify({ resultCountList }),
+            body: JSON.stringify({ resultCountList: resultCountList.map(item => item.app) }),
         };
     } catch (err) {
         return {
